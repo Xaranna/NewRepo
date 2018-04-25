11ff686f02f056408333048c51219d9d7076c8bd
Xaranna <vega_21_21@mail.ru>
2018-04-25 21:35:05 +0300

    com

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hello Nika!");
+            //Console.ReadKey();
         }
     }
 }
